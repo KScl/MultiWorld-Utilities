2a757eba4d49337035cef6ed3b52fde67e9401fc
Kaito Sinclaire <mattwcstrfan@gmail.com>
2022-01-18 05:26:03 -0800

    Baserom update to make text rendering faster

--- Rom.py
+++ Rom.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '1355bd5ded305552a4e31eb604640f14'
+RANDOMIZERBASEHASH = 'e565f989fd0fc64b5b6d875fe4a4c06d'
 
 import io
 import itertools
