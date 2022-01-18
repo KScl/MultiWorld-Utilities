fad07e5f2d3972a27681f2ece1b14b317f220509
Kaito Sinclaire <mattwcstrfan@gmail.com>
2022-01-17 18:38:03 -0800

    And sometimes, it's my own fault

--- Shops.py
+++ Shops.py
@@ -230,8 +230,8 @@ def ShopSlotFill(world):
                     else:
                         price = world.random.randrange(8, 56)
                     price *= 5
-                    if price > world.rupee_limit[player]: # unlikely failsafe
-                        price = world.rupee_limit[player]
+                    if price > world.rupee_limit[location.player]: # unlikely failsafe
+                        price = world.rupee_limit[location.player]
 
                     shop.push_inventory(int(location.name[-1]) - 1, item_name, price, 1,
                                         location.item.player if location.item.player != location.player else 0)
